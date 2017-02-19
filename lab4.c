af7aba670e1862d673a7138cd21ce2e6df4563eb
Marshall Lindsay <mbanks1050@gmail.com>
2017-02-18 19:53:51 -0600

    Updates 2/18/17

--- lab4.c
+++ lab4.c
@@ -10,7 +10,7 @@
 
 
 int checkCommandLineNumbers(char*);
-void openFile(int);
+FILE* openFile(int);
 void closeFile(int);
 
 
@@ -18,31 +18,48 @@ void closeFile(int);
 int main(int argc, char* argv[]){
 
 	int fileNumber;
-	fileNumber = atoi(argv[2]);
-	printf("\nThe first argument is %s", argv[0]);
-	printf("\nThe second argument is %s", argv[1]);
-	printf("\nThe third argument value is %d", fileNumber);
-	/*
-
+	FILE* fp = NULL;
+	
+/*
 	if(argc <3){
 		printf("\nInvalid number of command line arguements!");
 		return(1);
 	}
-	if((argv[1][0] != '-') && (argv[1][1] != 'n')){
-		printf("\nFirst command line argument is invalid!\n"
-				"Please specify the file number by -n [file number]");
-		return(1);
-	}
-	
-	fileNumber = atoi(argv[2]);
-	if(fileNumber){
-		printf("\nInvalid file number");
-		return(1);
-	}else{
-		printf("\n%d",fileNumber);
-	}
 */
+	
+	
+	if((argv[1][0] == '-') && (argv[1][1] == 'n')){
+		
+		fileNumber = atoi(argv[2]);
+		if(!fileNumber){
+			printf("\nIvalid file number");
+			return(1);
+		}
+
+	}else{
+		printf("\nFirst command line argument is invalid!\n"
+			"Please specify the file number by -n [file number]");
+		return(1);
+	}
+
+	fp = openFile(fileNumber);
+	if(fp == NULL){
+		printf("\nProblem opening file #%d. Terminating program", fileNumber);
+		return(1);
+	}
+
 	return(EXIT_SUCCESS);
 
+
+}
+
+FILE* openFile(int fileNumber){
+
+	switch(fileNumber){
+case 1:
+case 2:
+case 3:
+	
+	}
 }
 
